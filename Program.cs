245a74a9867df5dc846d46d98811fadef4069ab0
Siddhant Tandon <76514713+siddhantt24@users.noreply.github.com>
2024-07-25 22:41:20 +0530

    custom exception

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hey my name is Siddhant Tandon");
+﻿
+using System;
+namespace ExceptionHandlingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1, Number2, Result;
+            Console.WriteLine("Enter First Number:");
+            Number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number:");
+            Number2 = int.Parse(Console.ReadLine());
+            if (Number2 == 0)
+            {
+                Console.WriteLine("Second Number Should Not Be Zero");
+            }
+            else
+            {
+                Result = Number1 / Number2;
+                Console.WriteLine($"Result = {Result}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+// in this program if the second number entered is 0, so the program returns an custom exception instead of a error
